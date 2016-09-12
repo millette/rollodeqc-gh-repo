5340f33fd3287c6274ba64cc167de1f02491512a
Robin Millette <robin@millette.info>
2016-09-11 22:20:15 -0400

    404 test.

--- test.js
+++ test.js
@@ -6,3 +6,5 @@ test('title', async t => {
   const result = await fn('millette', 'rollodeqc-gh-repo')
   t.is(result.owner.login, 'millette')
 })
+
+test('404', async t => await t.throws(fn('millette/rollodeqc-gh-repo-404'), 'Response code 404 (Not Found)'))
